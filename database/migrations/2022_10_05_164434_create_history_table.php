ec72b31ae72af48c54233b532e65a75c42815f2c
cosof-daniel-huerta <105890740+cosof-daniel-huerta@users.noreply.github.com>
2022-10-06 04:06:16 -0400

    new

--- database/migrations/2022_10_05_164434_create_history_table.php
+++ database/migrations/2022_10_05_164434_create_history_table.php
@@ -14,6 +14,7 @@ class CreateHistoryTable extends Migration
         Schema::create('history', function (Blueprint $table) {
             $table->id();
             $table->string('usd');
+            $table->string('variation');
             $table->date('date');
             $table->time('hour');
         });
